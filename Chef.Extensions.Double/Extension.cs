7b143462dcebf5a1f3cb058f1414780afd5b78a8
johnny <johnny@wantgoo.com>
2018-09-24 00:04:00 +0800

    add Double extension method

---
+++ Chef.Extensions.Double/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chef.Extensions.Double
+{
+    public static class Extension
+    {
+        public static int Round(this double me, MidpointRounding mode = MidpointRounding.AwayFromZero)
+        {
+            return Convert.ToInt32(Math.Round(me, mode));
+        }
+
+        public static double Round(this double me, int digits, MidpointRounding mode = MidpointRounding.AwayFromZero)
+        {
+            return Math.Round(me, digits, mode);
+        }
+    }
+}
