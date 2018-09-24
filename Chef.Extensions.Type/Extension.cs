b70a8f8c41845ec5fe28d27146fefbfe0e0cc175
johnny <johnny@wantgoo.com>
2018-09-24 17:09:43 +0800

    update method name

--- Chef.Extensions.Type/Extension.cs
+++ Chef.Extensions.Type/Extension.cs
@@ -2,7 +2,7 @@
 {
     public static class Extension
     {
-        public static bool IsUserDefinedType(this System.Type me)
+        public static bool IsUserDefined(this System.Type me)
         {
             return !me.IsValueType && !me.IsPrimitive && (me.Namespace == null || !me.Namespace.StartsWith("System"));
         }
