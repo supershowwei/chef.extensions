da48d9970ded5f688732e756e8562c23eea29a24
johnny <johnny@wantgoo.com>
2018-10-02 09:32:56 +0800

    add Chef.Extensions.Boolean

---
+++ Chef.Extensions.Boolean/Extension.cs
@@ -0,0 +1,10 @@
+﻿namespace Chef.Extensions.Boolean
+{
+    public static class Extension
+    {
+        public static T IIF<T>(this bool me, T trueValue, T falseValue)
+        {
+            return me ? trueValue : falseValue;
+        }
+    }
+}
