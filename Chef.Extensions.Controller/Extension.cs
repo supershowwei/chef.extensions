e224ea84d0899d142c4351a5c38c041ab692aaca
johnny <johnny@wantgoo.com>
2018-11-21 20:58:39 +0800

    add Chef.Extensions.Controller

---
+++ Chef.Extensions.Controller/Extension.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace Chef.Extensions.Controller
+{
+    public static class Extension
+    {
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data)
+        {
+            return Jsonet(me, data, null, null, JsonRequestBehavior.DenyGet);
+        }
+
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data, string contentType)
+        {
+            return Jsonet(me, data, contentType, null, JsonRequestBehavior.DenyGet);
+        }
+
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data, string contentType, Encoding contentEncoding)
+        {
+            return Jsonet(me, data, contentType, contentEncoding, JsonRequestBehavior.DenyGet);
+        }
+
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data, JsonRequestBehavior behavior)
+        {
+            return Jsonet(me, data, null, null, behavior);
+        }
+
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data, string contentType, JsonRequestBehavior behavior)
+        {
+            return Jsonet(me, data, contentType, null, behavior);
+        }
+
+        public static JsonNetResult Jsonet(this System.Web.Mvc.Controller me, object data, string contentType, Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new JsonNetResult
+                   {
+                       Data = data,
+                       ContentType = contentType,
+                       ContentEncoding = contentEncoding,
+                       JsonRequestBehavior = behavior
+                   };
+        }
+    }
+}
---
+++ Chef.Extensions.Controller/JsonNetResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Chef.Extensions.Controller
+{
+    public class JsonNetResult : JsonResult
+    {
+        private static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            var response = context.HttpContext.Response;
+
+            response.ContentType = !string.IsNullOrEmpty(this.ContentType) ? this.ContentType : "application/json";
+
+            if (this.ContentEncoding != null) response.ContentEncoding = this.ContentEncoding;
+
+            if (this.Data != null)
+            {
+                var writer = new JsonTextWriter(response.Output);
+                var serializer = JsonSerializer.Create(SerializerSettings);
+
+                serializer.Serialize(writer, this.Data);
+                writer.Flush();
+            }
+        }
+    }
+}
