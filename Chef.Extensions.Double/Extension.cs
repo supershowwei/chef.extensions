9b22693ce1fb7ba9aa2691b3f3a183224eb6075b
johnny <johnny@wantgoo.com>
2019-01-21 22:31:08 +0800

    add some extension methods to Chef.Extensions.Double

--- Chef.Extensions.Double/Extension.cs
+++ Chef.Extensions.Double/Extension.cs
@@ -4,14 +4,38 @@ namespace Chef.Extensions.Double
 {
     public static class Extension
     {
-        public static int Round(this double me, MidpointRounding mode = MidpointRounding.AwayFromZero)
+        public static double Round(this double me, MidpointRounding mode = MidpointRounding.AwayFromZero)
         {
-            return Convert.ToInt32(Math.Round(me, mode));
+            return Math.Round(me, mode);
         }
 
         public static double Round(this double me, int digits, MidpointRounding mode = MidpointRounding.AwayFromZero)
         {
             return Math.Round(me, digits, mode);
+        }
+
+        public static int RoundUp(this double me)
+        {
+            return Convert.ToInt32(Math.Ceiling(me));
+        }
+
+        public static double RoundUp(this double me, int digits)
+        {
+            var power = Math.Pow(10, digits);
+
+            return Math.Ceiling(me * power) / power;
+        }
+
+        public static int RoundDown(this double me)
+        {
+            return Convert.ToInt32(Math.Floor(me));
+        }
+
+        public static double RoundDown(this double me, int digits)
+        {
+            var power = Math.Pow(10, digits);
+
+            return Math.Floor(me * power) / power;
         }
 
         public static int ToInt32(this double me)
