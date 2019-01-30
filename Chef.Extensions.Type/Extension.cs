cd723c50f8b8aa5d69d4f3afbbe70f6e8f847bbd
johnny <johnny@wantgoo.com>
2019-01-30 10:54:43 +0800

    add GetPropertyNames()

--- Chef.Extensions.Type/Extension.cs
+++ Chef.Extensions.Type/Extension.cs
@@ -1,10 +1,17 @@
-﻿namespace Chef.Extensions.Type
+﻿using System.Linq;
+
+namespace Chef.Extensions.Type
 {
     public static class Extension
     {
         public static bool IsUserDefined(this System.Type me)
         {
             return !me.IsValueType && !me.IsPrimitive && (me.Namespace == null || !me.Namespace.StartsWith("System"));
+        }
+
+        public static string[] GetPropertyNames(this System.Type me)
+        {
+            return me.GetProperties().Select(x => x.Name).ToArray();
         }
     }
 }
