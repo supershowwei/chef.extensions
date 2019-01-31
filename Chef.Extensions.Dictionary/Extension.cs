93de82c087f96bdd3f82987a760c744cbb1079d3
johnny <johnny@wantgoo.com>
2019-01-31 09:21:18 +0800

    update Chef.Extensions.Dictionary

--- Chef.Extensions.Dictionary/Extension.cs
+++ Chef.Extensions.Dictionary/Extension.cs
@@ -4,13 +4,27 @@ namespace Chef.Extensions.Dictionary
 {
     public static class Extension
     {
-        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> me, Dictionary<TKey, TValue> collection)
+        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> me, IDictionary<TKey, TValue> collection)
         {
-            if (collection == null || collection.Count == 0) return;
-
             foreach (var item in collection)
             {
                 me.Add(item.Key, item.Value);
+            }
+        }
+
+        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> me, ICollection<KeyValuePair<TKey, TValue>> collection)
+        {
+            foreach (var pair in collection)
+            {
+                me.Add(pair.Key, pair.Value);
+            }
+        }
+
+        public static void AddRange<TKey, TValue>(this Dictionary<TKey, TValue> me, IEnumerable<KeyValuePair<TKey, TValue>> collection)
+        {
+            foreach (var pair in collection)
+            {
+                me.Add(pair.Key, pair.Value);
             }
         }
     }
