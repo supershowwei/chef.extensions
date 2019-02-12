ee0d53abda9aa563c0056051e0fec2846ec76fe4
johnny <johnny@wantgoo.com>
2019-02-12 10:59:07 +0800

    add Chef.Extensions.Object

--- Chef.Extensions.Boolean/Extension.cs
+++ Chef.Extensions.Boolean/Extension.cs
@@ -1,10 +1,17 @@
-﻿namespace Chef.Extensions.Boolean
+﻿using System;
+
+namespace Chef.Extensions.Boolean
 {
     public static class Extension
     {
         public static T IIF<T>(this bool me, T trueValue, T falseValue)
         {
             return me ? trueValue : falseValue;
+        }
+
+        public static T IIF<T>(this bool me, Func<T> trueValue, Func<T> falseValue)
+        {
+            return me ? trueValue() : falseValue();
         }
     }
 }
---
+++ Chef.Extensions.Object/Extension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Chef.Extensions.Object
+{
+    public static class Extension
+    {
+        public static ExpandoObject ToExpando(this object me)
+        {
+            IDictionary<string, object> expando = new ExpandoObject();
+
+            foreach (var property in me.GetType().GetProperties())
+            {
+                expando.Add(property.Name, property.GetValue(me));
+            }
+
+            return (ExpandoObject)expando;
+        }
+    }
+}
