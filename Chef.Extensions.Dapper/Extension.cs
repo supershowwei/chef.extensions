f3f87d1e1b8df895820448344d24bd9429470d01
johnny <johnny@wantgoo.com>
2019-12-21 13:43:37 +0800

    update Chef.Extensions.Dapper, support Async/Await.

--- Chef.Extensions.Dapper/Extension.cs
+++ Chef.Extensions.Dapper/Extension.cs
@@ -10,6 +10,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Chef.Extensions.Dapper.Extensions;
 using Dapper;
 
@@ -62,6 +63,17 @@ namespace Chef.Extensions.Dapper
             }
         }
 
+        public static async Task<IEnumerable<T>> PolymorphicQueryAsync<T>(
+            this IDbConnection cnn,
+            string sql,
+            object param = null,
+            string discriminator = "Discriminator")
+        {
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            return reader.PolymorphicExecuteReaderSync<T>(sql, discriminator);
+        }
+
         public static T PolymorphicQuerySingle<T>(
             this IDbConnection cnn,
             string sql,
@@ -72,6 +84,34 @@ namespace Chef.Extensions.Dapper
             var count = 0;
 
             using (var reader = cnn.ExecuteReader(sql, param))
+            {
+                while (reader.Read())
+                {
+                    if (++count > 1) throw new InvalidOperationException("Sequence contains more than one element.");
+
+                    var parser = RowParserProvider.GetRowParser<T>(discriminator, reader, sql);
+
+                    result = parser(reader);
+                }
+            }
+
+            if (count == 0) throw new InvalidOperationException("Sequence contains no elements.");
+
+            return result;
+        }
+
+        public static async Task<T> PolymorphicQuerySingleAsync<T>(
+            this IDbConnection cnn,
+            string sql,
+            object param = null,
+            string discriminator = "Discriminator")
+        {
+            var result = default(T);
+            var count = 0;
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
             {
                 while (reader.Read())
                 {
@@ -112,6 +152,32 @@ namespace Chef.Extensions.Dapper
             return result;
         }
 
+        public static async Task<T> PolymorphicQuerySingleOrDefaultAsync<T>(
+            this IDbConnection cnn,
+            string sql,
+            object param = null,
+            string discriminator = "Discriminator")
+        {
+            var result = default(T);
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
+            {
+                var count = 0;
+                while (reader.Read())
+                {
+                    if (++count > 1) throw new InvalidOperationException("Sequence contains more than one element.");
+
+                    var parser = RowParserProvider.GetRowParser<T>(discriminator, reader, sql);
+
+                    result = parser(reader);
+                }
+            }
+
+            return result;
+        }
+
         public static T PolymorphicQueryFirst<T>(
             this IDbConnection cnn,
             string sql,
@@ -119,6 +185,27 @@ namespace Chef.Extensions.Dapper
             string discriminator = "Discriminator")
         {
             using (var reader = cnn.ExecuteReader(sql, param))
+            {
+                while (reader.Read())
+                {
+                    var parser = RowParserProvider.GetRowParser<T>(discriminator, reader, sql);
+
+                    return parser(reader);
+                }
+            }
+
+            throw new InvalidOperationException("Sequence contains no elements.");
+        }
+
+        public static async Task<T> PolymorphicQueryFirstAsync<T>(
+            this IDbConnection cnn,
+            string sql,
+            object param = null,
+            string discriminator = "Discriminator")
+        {
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
             {
                 while (reader.Read())
                 {
@@ -154,9 +241,39 @@ namespace Chef.Extensions.Dapper
             return result;
         }
 
+        public static async Task<T> PolymorphicQueryFirstOrDefaultAsync<T>(
+            this IDbConnection cnn,
+            string sql,
+            object param = null,
+            string discriminator = "Discriminator")
+        {
+            var result = default(T);
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    var parser = RowParserProvider.GetRowParser<T>(discriminator, reader, sql);
+
+                    result = parser(reader);
+
+                    break;
+                }
+            }
+
+            return result;
+        }
+
         public static void PolymorphicInsert(this IDbConnection cnn, string sql, object param)
         {
             HierarchyInsert(cnn, sql, param);
+        }
+
+        public static Task PolymorphicInsertAsync(this IDbConnection cnn, string sql, object param)
+        {
+            return HierarchyInsertAsync(cnn, sql, param);
         }
 
         public static void HierarchyInsert(this IDbConnection cnn, string sql, object param)
@@ -186,6 +303,33 @@ namespace Chef.Extensions.Dapper
             }
         }
 
+        public static Task HierarchyInsertAsync(this IDbConnection cnn, string sql, object param)
+        {
+            if (param == null) throw new ArgumentException($"'{nameof(param)}' is null.");
+
+            var props = Regex.Matches(sql, "@([^\\s,)]+)").Cast<Match>().Select(m => m.Groups[1].Value).ToList();
+
+            if (props.Count == 0) throw new ArgumentException($"'{nameof(sql)}' has no parameters.");
+
+            if (param is IEnumerable objs)
+            {
+                var parameters = new List<object>();
+
+                foreach (var obj in objs)
+                {
+                    parameters.Add(GetParameter(props, obj));
+                }
+
+                if (!parameters.Any()) throw new ArgumentException($"'{nameof(param)}' is empty.");
+
+                return cnn.ExecuteAsync(sql, parameters);
+            }
+            else
+            {
+                return cnn.ExecuteAsync(sql, GetParameter(props, param));
+            }
+        }
+
         public static IEnumerable<T> QueryAsImmutability<T>(this IDbConnection cnn, string sql, object param = null)
         {
             using (var reader = cnn.ExecuteReader(sql, param))
@@ -199,12 +343,43 @@ namespace Chef.Extensions.Dapper
             }
         }
 
+        public static async Task<IEnumerable<T>> QueryAsImmutabilityAsync<T>(this IDbConnection cnn, string sql, object param = null)
+        {
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            return reader.ExecuteReaderAsImmutabilitySync<T>(sql);
+        }
+
         public static T QuerySingleAsImmutability<T>(this IDbConnection cnn, string sql, object param = null)
         {
             var result = default(T);
             var count = 0;
 
             using (var reader = cnn.ExecuteReader(sql, param))
+            {
+                while (reader.Read())
+                {
+                    if (++count > 1) throw new InvalidOperationException("Sequence contains more than one element.");
+
+                    var constructor = typeof(T).GetConstructors().First();
+
+                    result = (T)Activator.CreateInstance(typeof(T), constructor.GetArguments(reader));
+                }
+            }
+
+            if (count == 0) throw new InvalidOperationException("Sequence contains no elements.");
+
+            return result;
+        }
+
+        public static async Task<T> QuerySingleAsImmutabilityAsync<T>(this IDbConnection cnn, string sql, object param = null)
+        {
+            var result = default(T);
+            var count = 0;
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
             {
                 while (reader.Read())
                 {
@@ -241,9 +416,48 @@ namespace Chef.Extensions.Dapper
             return result;
         }
 
+        public static async Task<T> QuerySingleOrDefaultAsImmutabilityAsync<T>(this IDbConnection cnn, string sql, object param = null)
+        {
+            var result = default(T);
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
+            {
+                var count = 0;
+                while (reader.Read())
+                {
+                    if (++count > 1) throw new InvalidOperationException("Sequence contains more than one element.");
+
+                    var constructor = typeof(T).GetConstructors().First();
+
+                    result = (T)Activator.CreateInstance(typeof(T), constructor.GetArguments(reader));
+                }
+            }
+
+            return result;
+        }
+
         public static T QueryFirstAsImmutability<T>(this IDbConnection cnn, string sql, object param = null)
         {
             using (var reader = cnn.ExecuteReader(sql, param))
+            {
+                while (reader.Read())
+                {
+                    var constructor = typeof(T).GetConstructors().First();
+
+                    return (T)Activator.CreateInstance(typeof(T), constructor.GetArguments(reader));
+                }
+            }
+
+            throw new InvalidOperationException("Sequence contains no elements.");
+        }
+
+        public static async Task<T> QueryFirstAsImmutabilityAsync<T>(this IDbConnection cnn, string sql, object param = null)
+        {
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
             {
                 while (reader.Read())
                 {
@@ -261,6 +475,27 @@ namespace Chef.Extensions.Dapper
             var result = default(T);
 
             using (var reader = cnn.ExecuteReader(sql, param))
+            {
+                while (reader.Read())
+                {
+                    var constructor = typeof(T).GetConstructors().First();
+
+                    result = (T)Activator.CreateInstance(typeof(T), constructor.GetArguments(reader));
+
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static async Task<T> QueryFirstOrDefaultAsImmutabilityAsync<T>(this IDbConnection cnn, string sql, object param = null)
+        {
+            var result = default(T);
+
+            var reader = await cnn.ExecuteReaderAsync(sql, param);
+
+            using (reader)
             {
                 while (reader.Read())
                 {
@@ -483,6 +718,32 @@ namespace Chef.Extensions.Dapper
             sb.Remove(sb.Length - 2, 2);
 
             return sb.ToString();
+        }
+
+        private static IEnumerable<T> PolymorphicExecuteReaderSync<T>(this IDataReader me, string sql, string discriminator)
+        {
+            using (me)
+            {
+                while (me.Read())
+                {
+                    var parser = RowParserProvider.GetRowParser<T>(discriminator, me, sql);
+
+                    yield return parser(me);
+                }
+            }
+        }
+
+        private static IEnumerable<T> ExecuteReaderAsImmutabilitySync<T>(this IDataReader me, string sql)
+        {
+            using (me)
+            {
+                while (me.Read())
+                {
+                    var constructor = typeof(T).GetConstructors().First();
+
+                    yield return (T)Activator.CreateInstance(typeof(T), constructor.GetArguments(me));
+                }
+            }
         }
 
         private static ExpandoObject GetParameter(List<string> props, object obj)
