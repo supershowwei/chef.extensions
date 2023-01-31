5239b04b4b29c0d9b780c571a63a8c04e6f4c0c2
johnny <johnny@wantgoo.com>
2023-01-31 09:31:41 +0800

    add MediaTypes.cs

---
+++ Chef.Extensions.String/MediaTypes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Chef.Extensions.String
+{
+    // https://www.iana.org/assignments/media-types/media-types.xhtml
+    public class MediaTypes
+    {
+        private static readonly Dictionary<string, string> Types = new Dictionary<string, string>
+                                                                   {
+                                                                       ["json"] = "application/json",
+                                                                       ["gif"] = "image/gif",
+                                                                       ["jpeg"] = "image/jpeg",
+                                                                       ["png"] = "image/png",
+                                                                       ["css"] = "text/css",
+                                                                       ["csv"] = "text/csv",
+                                                                       ["html"] = "text/html",
+                                                                       ["javascript"] = "text/javascript",
+                                                                       ["plain"] = "text/plain",
+                                                                       ["xml"] = "text/xml"
+                                                                   };
+
+        public static string Json => GetMediaType("json");
+
+        public static string Html => GetMediaType("html");
+
+        public static string Plain => GetMediaType("plain");
+
+        public static string GetMediaType(string name)
+        {
+            return Types[name];
+        }
+    }
+}
